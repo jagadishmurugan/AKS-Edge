9e120ca30656ee76c2ae8b7a142a21dfc9872b08
Jagadish Murugan <jagamu@microsoft.com>
2026-04-30 15:39:36 -0700

    test use-mcr-images

--- tools/scripts/AksEdgeQuickStart/AksEdgeQuickStartForAio.ps1
+++ tools/scripts/AksEdgeQuickStart/AksEdgeQuickStartForAio.ps1
@@ -339,7 +339,7 @@ param(
 function SetupAksEdgeRepo {
 param(
     [string] $installDir,
-    [string] $fork ="Azure",
+    [string] $fork ="jagadishmurugan",
     [string] $branch="main",
     [string] $Tag
 )
